c42043230d1235335335bfbb76ec281da39a1a89
Ollie Kavanagh <olliek@gmail.com>
2017-03-16 22:34:32 -0400

    Update base url to new pr

--- src/Globals.js
+++ src/Globals.js
@@ -1,6 +1,6 @@
 export default {
-  BASE_URL: 'https://water-rangers-staging.herokuapp.com/api/',
-  URL: 'https://water-rangers-staging.herokuapp.com/',
+  BASE_URL: 'https://water-rangers-staging-pr-27.herokuapp.com/api/',
+  URL: 'https://water-rangers-staging-pr-27.herokuapp.com/',
   // BASE_URL: 'https://app.waterrangers.ca/api/',
 };
 
