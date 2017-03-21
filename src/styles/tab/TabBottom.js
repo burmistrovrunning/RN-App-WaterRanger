420c5309e79caf17072395d6bf705346d1d4f26a
Ollie Kavanagh <olliek@gmail.com>
2017-03-21 11:44:18 -0400

    Android styling updates

--- src/styles/tab/TabBottom.js
+++ src/styles/tab/TabBottom.js
@@ -9,17 +9,30 @@ export const styles = EStyleSheet.create({
     borderTopWidth: 0,
     flexDirection: 'row',
     height: 49,
-    paddingVertical: 3
+    paddingVertical: 3,
+    '@media android': {
+      borderTopWidth: 1,
+      borderTopColor: '#264364',
+      overflow: 'visible'
+    },
   },
   tabContainer: {
     alignItems: 'center',
     flex: 1,
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    '@media android': {
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
   },
   tabLabel: {
     color: '$colorWhite',
     fontSize: 10,
-    fontFamily: '$globalFont'
+    fontFamily: '$globalFont',
+    '@media android': {
+      fontSize: 12,
+      fontWeight: 'bold'
+    },
   },
   tabIcon: {
     color: '$colorWhite',
@@ -27,6 +40,12 @@ export const styles = EStyleSheet.create({
     height: 30,
     marginTop: 0,
     textAlign: 'center',
-    width: 30
+    width: 30,
+    '@media android': {
+      height: 25,
+      fontSize: 25,
+      marginRight: 5,
+      width: 25
+    },
   },
 });
