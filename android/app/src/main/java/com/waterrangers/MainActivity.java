3a72eb0e1c0cfeae310f17e75dd0cb82b6176f74
Keith Turner <flooglepop@gmail.com>
2017-03-24 19:26:54 -0500

    Configure android for facebook sdk

--- android/app/src/main/java/com/waterrangers/MainActivity.java
+++ android/app/src/main/java/com/waterrangers/MainActivity.java
@@ -3,12 +3,18 @@ package com.waterrangers;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.cboy.rn.splashscreen.SplashScreen;
 import android.app.Activity;
 import android.os.Handler;
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
