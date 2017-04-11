fd2b207e7579596f0d6b49139d30cdb3cfe62ba0
nick_hagenov <nicoloas.hagenovo327@gmail.com>
2017-04-11 10:50:46 +0300

    Fix erro #52.
    Update react native version
    Enable multi-dex enable on application. (Below lollipop, we need to do this for `multiDexEnabled=true` case

--- android/app/src/main/java/com/waterrangers/MainApplication.java
+++ android/app/src/main/java/com/waterrangers/MainApplication.java
@@ -1,6 +1,8 @@
 package com.waterrangers;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
@@ -73,5 +75,10 @@ public class MainApplication extends Application implements ReactApplication {
     AppEventsLogger.activateApp(this);
     SoLoader.init(this, /* native exopackage */ false);
 
+  }
+  @Override
+  protected void attachBaseContext(Context base) {
+    super.attachBaseContext(base);
+    MultiDex.install(this);
   }
 }
